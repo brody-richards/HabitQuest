dc65f94ebf80149005fc5fb2d8dafedbd270d08a
brodyedwardrichards <brody.richards@edu.sait.ca>
2025-02-12 09:42:13 -0700

    set up OpenSSL Certificates

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const http = require('http');
+const hsts = require('hsts');
+
+const app = express();
+const PORT_HTTP = 3000;
+const PORT_HTTPS = 3443;
+
+app.get('/', (req, res) => {
+    res.send('HTTP Quest Tracker');
+});
+
+app.get('/secure', (req, res) => {
+    res.send('HTTPS Quest Tracker');
+});
+
+const hstsOptions = {
+    maxAge: 31536000, 
+    includeSubDomains: true, 
+    preload: true 
+};
+
+http.createServer(app).listen(PORT_HTTP, () => {
+    console.log(`HTTP Server running at http://localhost:${PORT_HTTP}`);
+});
+
+const options = {
+    key: fs.readFileSync('private-key.pem'),
+    cert: fs.readFileSync('certificate.pem'),
+};
+
+const httpsServer = https.createServer(options, (req, res) => {
+    hsts(hstsOptions)(req, res, () => {
+        app(req, res);
+    });
+});
+
+httpsServer.listen(PORT_HTTPS, () => {
+    console.log(`HTTPS Server running at https://localhost:${PORT_HTTPS}`);
+});
